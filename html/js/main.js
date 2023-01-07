46744f415b45aa26a7748bfcf191010a83e261d5
Demid Yermak <d.yermak@artjoker.net>
2023-01-07 02:28:13 +0200

    Finish header

--- html/js/main.js
+++ html/js/main.js
@@ -2,6 +2,18 @@
 
 $(function () {
   $('.js-button-dropdown').on('click', function () {
-    $(this).closest('.js-dropdown-block').toggleClass('is-open');
+    $(this).toggleClass('is-active').closest('.js-dropdown-block').find('.js-dropdown-item').toggleClass('is-open');
+  });
+  function burger(button) {
+    var overlay = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '.js-overlay';
+    $(button).toggleClass('is-active');
+    $('.js-header-modal').toggleClass('is-open');
+    $(overlay).toggleClass('is-visible');
+  }
+  $('.js-burger-menu').on('click', function () {
+    burger(this);
+  });
+  $('.js-overlay').on('click', function () {
+    burger('.js-burger-menu', this);
   });
 });
