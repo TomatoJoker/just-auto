473e517fd263cdb99f3949de242d1f933396c552
Demid Yermak <d.yermak@artjoker.net>
2023-01-09 00:18:28 +0200

    Add slider desktop

--- html/js/main.js
+++ html/js/main.js
@@ -1,5 +1,12 @@
 "use strict";
 
+var slideHeroLenght = jQuery('.js-hero-slide').length,
+  counterHtml = '';
+// console.log(slideHeroLenght);
+for (var i = 1; i <= slideHeroLenght; i++) {
+  counterHtml += "<li class=\"hero__count js-counter-slide\"><span>0".concat(i, "</span></li>");
+}
+$('.js-counter').html(counterHtml);
 $(function ($) {
   $('.js-button-dropdown').on('click', function () {
     $(this).toggleClass('is-active').closest('.js-dropdown-block').find('.js-dropdown-item').toggleClass('is-open');
@@ -21,9 +28,51 @@ $(function ($) {
     smallBtn: false,
     toolbar: false
   });
-  $('.js-hero-slider').slick({
+  var counter = '.js-counter',
+    heroSlider = '.js-hero-slider',
+    startSlide = 2,
+    counterItem = '.js-counter-slide',
+    dotsContainer = '.js-dots',
+    getSpeed = getComputedStyle(document.documentElement).getPropertyValue('--speed'),
+    timeFunction = getComputedStyle(document.documentElement).getPropertyValue('--time-function'),
+    speed = getSpeed.slice(0, getSpeed.length - 2);
+  console.log(speed);
+  // console.log(typeof timeFunction);
+  $(heroSlider).slick({
+    infinite: false,
     slidesToShow: 1,
     slidesToScroll: 1,
-    draggable: true
+    draggable: true,
+    initialSlide: startSlide - 1,
+    asNavFor: counter,
+    appendArrows: dotsContainer,
+    appendDots: dotsContainer,
+    dots: true,
+    dotsClass: 'hero__dots',
+    swipe: false,
+    speed: speed * 1.3,
+    cssEase: timeFunction,
+    prevArrow: '<button class="hero__arrow hero__arrow-left"><svg class="icon" width="14" height="24" viewBox="0 0 14 24" xmlns="http://www.w3.org/2000/svg"><path d="M13.034 23.046a1.77 1.77 0 0 0 0-2.502l-8.762-8.762 8.761-8.761A1.77 1.77 0 0 0 10.532.518L.518 10.531a1.77 1.77 0 0 0 0 2.503l10.013 10.012a1.77 1.77 0 0 0 2.502 0Z"/></svg></button>',
+    nextArrow: '<button class="hero__arrow hero__arrow-right"><svg class="icon" width="14" height="24" viewBox="0 0 14 24" xmlns="http://www.w3.org/2000/svg"><path d="M.518.518a1.77 1.77 0 0 0 0 2.503l8.761 8.761-8.76 8.761a1.77 1.77 0 0 0 2.502 2.503l10.012-10.012a1.77 1.77 0 0 0 0-2.503L3.021.518a1.77 1.77 0 0 0-2.503 0Z"/></svg></button>'
   });
+  $(counter).slick({
+    vertical: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    initialSlide: startSlide - 1,
+    swipe: false,
+    touchMove: false
+  });
+  counterClick(counter);
+  counterClick(heroSlider);
+  function counterClick(slider) {
+    var el = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : counterItem;
+    $("".concat(el)).on("click", function () {
+      $(counterItem).removeClass('slick-current');
+      var index = $(this).attr("data-slick-index");
+      $(slider).slick("slickGoTo", index);
+      $(counterItem).eq(index).addClass('slick-current');
+    });
+  }
 });
